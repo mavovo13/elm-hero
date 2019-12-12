412d599b1bef33bf5319f10619f287775f67ce29
masamasah <masayuki.h0729@gmail.com>
2019-12-12 21:18:26 +0900

    The Hero Editor
    implement hero editor by Elm.
    https://angular.io/tutorial/toh-pt1

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,57 @@
+module Main exposing (Hero, Model, Msg(..), main, update, view)
+
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, h1, h2, input, span, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
 
 type Msg
-    = NoOp
+    = Change String
 
+
+type alias Hero =
+    { name : String, id : Int }
+
+
+type alias Model =
+    { hero : Hero
+    , title : String
+    }
+
+
+init : Model
+init =
+    let
+        hero =
+            { id = 1, name = "Windstorm" }
+    in
+    { hero = hero, title = "Tour of Heroes" }
+
+
+update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp ->
-      model + 1
+    case msg of
+        Change newName ->
+            let
+                newHero =
+                    { id = model.hero.id, name = newName }
+            in
+            { model | hero = newHero }
 
+
+view : Model -> Html Msg
 view model =
-  div []
-    [div [] [ text "hello"]
-    ]
+    div []
+        [ h1 [] [ text model.title ]
+        , h2 [] [ text (model.hero.name |> String.toUpper) ]
+        , div [] [ span [] [ text "id:" ], text (model.hero.id |> String.fromInt) ]
+        , div []
+            [ text "name:"
+            , input [ value model.hero.name, onInput Change ] []
+            ]
+        ]
